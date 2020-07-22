dbf0fe5467b8bfa405923fa7a85858509a99dfa7
nitunwa <nitun.2@gmail.com>
2020-07-22 15:15:23 -0700

    add Edit option

--- src/actions/countryByApi.js
+++ src/actions/countryByApi.js
@@ -18,19 +18,64 @@ export const loadDataByCountry = (placeData, country) => {
 //country deatails API call 
 export const loadDataByCountryDetails = (cityDetailsData, cityId) => {
   console.log("City id ---  " + cityId);
+
   return fetch(`http://localhost:5009/api/Places/${cityId}`)
+    .then(response => response.json())
+    .then(
+      (jsonifiedResponse) => {
+        // console.log("jsonifiedResponse  ");
+        // console.log(jsonifiedResponse);
+        //cityDetailsData(jsonifiedResponse);
+        return jsonifiedResponse;
+      })
+    .catch((error) => {
+      return error
+    });
+
+};
+
+// get review call
+
+export const loadReviewData = (cityName, token) => {
+  console.log("City name ---  " + cityName);
+  console.log("token ---  " + token);
+
+  const requestOptions = {
+    headers: { 'Content-Type': 'application/json', 'Authorization': 'bearer ' + token }
+  };
+  // http://localhost:5009/api/Reviews/getreviews?city=London
+  return fetch(`http://localhost:5009/api/Reviews/getreviews?city=${cityName}`, requestOptions)
     .then(response => response.json())
     .then(
       (jsonifiedResponse) => {
         console.log("jsonifiedResponse  ");
         console.log(jsonifiedResponse);
-        cityDetailsData(jsonifiedResponse);
+        return jsonifiedResponse
       })
     .catch((error) => {
-      cityDetailsData(error);
+      return error
     });
 
 };
+
+
+export const loadCityData = (cityDetailsData, cityId, token) => {
+  console.log("async");
+  let promise = loadDataByCountryDetails(cityDetailsData, cityId);
+
+  console.log("token:" + token)
+  promise.then(cityData =>
+    loadReviewData(cityData.city, token).then(
+      reviewData => {
+        cityDetailsData(Object.assign({}, cityData, { reviews: reviewData }));
+      }
+    )
+  )
+
+
+}
+
+
 
 //review post API call 
 export const postReview = (reviewData, review, placeId, token) => {
--- src/comoponents/CountryDetails.js
+++ src/comoponents/CountryDetails.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useLocation } from 'react-router';
 import { loadDataByCountryDetails } from './../actions/countryByApi'
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { postReview } from './../actions/countryByApi'
+import { useHistory, Redirect } from 'react-router-dom';
+import { postReview, loadCityData } from './../actions/countryByApi'
 
 
 const CountryDetails = () => {
@@ -12,24 +12,23 @@ const CountryDetails = () => {
   const { cityId } = useParams();
   const [city, setState] = useState({});
   const token = useSelector(state => state.user.userInfo.token);
+  const userId = useSelector(state => state.user.userInfo.id);
+  const loginStatus = useSelector(state => state.user.login);
+
 
   const cityDetailsData = (data) => {
     console.log("city data");
-    console.log(data.reviews.length);
-
+    //  console.log(data.reviews.length);
     setState(data);
 
   }
   const reviewData = (data) => {
-    console.log(" post review");
+    console.log(" post review....");
     // console.log(data);
-    loadDataByCountryDetails(cityDetailsData, cityId);
+    loadCityData(cityDetailsData, cityId, token);
+
   };
-
-
   const doComment = (event) => {
-
-
 
     event.preventDefault();
     if (token == null) {
@@ -40,23 +39,33 @@ const CountryDetails = () => {
       postReview(reviewData, event.target.review.value, city.placeId, token);
     }
 
-
-
   }
 
-
-  //let cityArr = [];
   useEffect(() => {
     // Update the document title using the browser API
-    loadDataByCountryDetails(cityDetailsData, cityId);
+    //  loadDataByCountryDetails(cityDetailsData, cityId);
+    loadCityData(cityDetailsData, cityId, token);
   }, [cityId]);
 
   return (
     <React.Fragment>
+
+      {!loginStatus ? <Redirect to={`/signin${location.pathname}`} /> : <h3></h3>}
+
       <h3>{city.city} Details</h3>
       <h3>City Rating : {city.rating} </h3>
       <h4>Reviews</h4>
-      {city.reviews?.map(review => <li>{review.reviewText}</li>)}
+      {
+
+        city.reviews?.map(review => <li>{review.reviewText}
+
+          {review.user.id === userId ? <a href="#">  Edit </a> : ''}
+        </li>)
+
+      }
+
+
+
 
       <h5>Share your reviews</h5>
       <form onSubmit={doComment.bind(this)}>
